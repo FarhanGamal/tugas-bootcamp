2dc7c1d82e229a14dfb25538e37ca88d2e287fd2
FarhanGamal <farhangamal78@gmail.com>
2024-12-09 17:54:38 +0700

    membuat tugas 15 dan mengedit tudas 14

--- tugas14/nilai_mahasiswa.js
+++ tugas14/nilai_mahasiswa.js
@@ -4,12 +4,15 @@ import Nilai from './nilai.js'
 class nilaiMahasiswa {
     constructor (id, nim, nama, jenis_kelamin, ipk, mata_kuliah, nilai) {
         this.id = id;
-        this.nim = nim;
-        this.nama = nama;
-        this.jenis_kelamin = jenis_kelamin;
-        this.ipk = ipk;
-        this.mata_kuliah = mata_kuliah;
-        this.nilai = nilai;
+        // this.nim = nim;
+        // this.nama = nama;
+        // this.jenis_kelamin = jenis_kelamin;
+        // this.ipk = ipk;
+        // this.mata_kuliah = mata_kuliah;
+        // this.nilai = nilai;
+
+        this.mhs = new Mahasiswa(nim, nama, jenis_kelamin, ipk);
+        this.nilai = new Nilai(mata_kuliah, nilai);
     }
 }
 
